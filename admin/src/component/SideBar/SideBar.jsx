8c7dd5c6528b59c8d96a38d94f4126d53e062bf4
taj <tajinder@gmail.com>
2024-10-22 19:13:53 +0100

    file name updated

--- admin/src/component/SideBar/SideBar.jsx
+++ admin/src/component/SideBar/SideBar.jsx
@@ -1,6 +1,6 @@
 import  { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './Sidebar.css';
+import './SideBar.css';
 import Logout from '../logout/Logout'
 const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(false);
