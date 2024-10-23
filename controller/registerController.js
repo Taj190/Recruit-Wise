4620259c19a260aff04bf7a4ef3345da0d46b637
taj <tajinder@gmail.com>
2024-10-23 14:12:39 +0100

    login controller updated

--- controller/registerController.js
+++ controller/registerController.js
@@ -47,7 +47,7 @@ export const loginController = async(req, res)=>{
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV,
-      sameSite: 'Strict',
+      sameSite: 'None',
     
   });
     res.status(200).send({
