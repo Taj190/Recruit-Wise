8502587777aa4f629bcb8b9c747336450d45e4eb
taj <tajinder@gmail.com>
2024-10-23 12:28:04 +0100

    jobdata route updated

--- route/jobData.js
+++ route/jobData.js
@@ -3,7 +3,7 @@ import { isAdmin, isLoggedIn } from '../middleware/middle.js';
 import { deleteUserDataController, editUserDataController, getUserDataController } from '../controller/jobController.js';
 export const job = express.Router();
 
-job.get('/userData' ,isLoggedIn,isAdmin , getUserDataController)
+job.get('/userData' ,isAdmin , getUserDataController)
 job.delete('/userdata/:id', isLoggedIn,isAdmin,deleteUserDataController)
 job.put('/edituserdata/:id', isLoggedIn,isAdmin,editUserDataController)
 
