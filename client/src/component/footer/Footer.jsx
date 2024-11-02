56850496f83952df6252c1742e6725183597a112
taj <tajinder@gmail.com>
2024-11-02 12:35:58 +0000

    navbar color changed

--- client/src/component/footer/Footer.jsx
+++ client/src/component/footer/Footer.jsx
@@ -9,13 +9,16 @@ const Footer = () => {
       <div className="container">
         <div className="footer-content">
           <div className="footer-logo">
-            <img src={logo} alt="Logo" className="footer-logo-image" />
+            <izmg src={logo} alt="Logo" className="footer-logo-image" />
             <p><span className='recruit'>Recruit</span>-<span className='wise'>Wise</span></p>
           </div>
           <div className="footer-info">
             <div className="contact">
               <p>
                 <MailOutlined /> Email: <a href="mailto:geral@recruitwise.pt">geral@recruitwise.pt</a>
+              </p>
+              <p>
+                <MailOutlined /> For Job Enquiry : <a href="mailto:rh@recruitwise.pt">rh@recruitwise.pt</a>
               </p>
               <p>
                 <PhoneOutlined /> Phone: <span>243073019</span>
@@ -33,11 +36,10 @@ const Footer = () => {
           <div className="map-container">
             <h3>Our Location</h3>
             <div className="map-wrapper">
-              <iframe
-               title="Google Map"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3085.5288928450805!2d-8.940634324187231!3d39.34425797163209!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd18b8da28815651%3A0x520cc82ff9eb04b1!2sR.%20Mariano%20de%20Carvalho%2C%20Rio%20Maior!5e0!3m2!1sen!2spt!4v1729280768457!5m2!1sen!2spt"  width="400"  
-              height="300" 
-              style={{ border: 0 }}
+              <iframe className = 'map'
+              title="Google Map"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3085.7581419500684!2d-8.938825924187476!3d39.339064871633184!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd18b8c5520baed1%3A0x504751fd9b93e738!2sR.%20Mariano%20de%20Carvalho%2026%2C%20Rio%20Maior!5e0!3m2!1sen!2spt!4v1730550208098!5m2!1sen!2spt" width="400"  
+              height="300"  style={{ border: 0 }} 
               allowFullScreen="" loading="lazy"></iframe>
             </div>
           </div>
